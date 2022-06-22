0c8811556fba975049ad9e9687beb01856a6f4db
mohakhlf@ <mohakhellafi@gmail.com>
2022-06-22 14:48:52 +0200

    refactor(delete artifact): :art: replace request url by methods of api github

--- scripts/delete-atifacts.js
+++ scripts/delete-atifacts.js
@@ -1,25 +1,21 @@
 module.exports = async({ github, context }) => {
-    const artifacts_responds = await github.request(
-        `GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-        }
-    )
+    // const artifacts_responds = await github.request(
+    //     `GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
+    //         owner: context.repo.owner,
+    //         repo: context.repo.repo,
+    //     }
+    // )
+    const artifacts_responds = await github.rest.actions.listArtifactsForRepo({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    });
 
     const artifacts_list_with_develop = artifacts_responds.data.artifacts.filter(
         (artifact) => artifact.name === 'main'
     )
     artifacts_list_with_develop.shift()
     console.log('artifacts_list_with_develop', artifacts_list_with_develop)
-    artifacts_list_with_develop.map(async(artifact) => {
-        console.log('artifact', artifact)
-            // await github.request(
-            //     `DELETE /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts/${artifact.id}`, {
-            //         owner: context.repo.owner,
-            //         repo: github.repository,
-            //         artifact_id: artifact.id,
-            //     }
-            // )
+    artifacts_list_with_develop.forEach(async(artifact) => {
         await github.rest.actions.deleteArtifact({
             owner: context.repo.owner,
             repo: context.repo.repo,
