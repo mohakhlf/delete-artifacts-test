9499ee6ead409cd3fda1351e4eee6405c0fd61a1
mohakhlf@ <mohakhellafi@gmail.com>
2022-06-22 11:19:15 +0200

    feat(delete artifact): :sparkles: check what's in artifact

--- scripts/delete-atifacts.js
+++ scripts/delete-atifacts.js
@@ -1,17 +1,24 @@
 module.exports = async({ github, context }) => {
-    const artifacts_list = await github.request(`GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
-        owner: context.repo.owner,
-        repo: context.repo.repo,
+    const artifacts_list = await github.request(
+        `GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+        }
+    )
+
+    const artifacts_list_with_develop = artifacts_list.filter(
+        (artifact) => artifact.name === 'main'
+    )
+    artifacts_list_with_develop.shift()
+    console.log('artifacts_list_with_develop', artifacts_list_with_develop)
+    artifacts_list_with_develop.map(async(artifact) => {
+        console.log('artifact', artifact)
+            // await github.request(
+            //     `DELETE /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts/${artifact.id}`, {
+            //         owner: context.repo.owner,
+            //         repo: context.repo.repo,
+            //         artifact_id: artifact.id,
+            //     }
+            // )
     })
-
-    const artifacts_to_delete = artifacts_list.data.artifacts.shift()
-
-    console.log('artifacts_list', artifacts_list.data.artifacts);
-    console.log('artifacts_to_delete', artifacts_to_delete);
-
-    // await github.request(`DELETE /repos/{owner}/{repo}/actions/artifacts/{artifact_id}`, {
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    //     artifact_id: 'ARTIFACT_ID'
-    // })
 }
