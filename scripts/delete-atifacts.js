7f795afda64a53f66a2ba183879d59b853f63f6e
mohakhlf@ <mohakhellafi@gmail.com>
2022-06-22 08:11:02 +0200

    feat(delete artifacts): :sparkles: list artifacts of repo

--- scripts/delete-atifacts.js
+++ scripts/delete-atifacts.js
@@ -1,8 +1,8 @@
 module.exports = async({ github, context }) => {
-    // const artifacts_list = await github.request(`GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    // })
-    console.log('github', github)
-        // console.log('artifacts_list', artifacts_list);
+    const artifacts_list = await github.request(`GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    })
+
+    console.log('artifacts_list', artifacts_list);
 }
