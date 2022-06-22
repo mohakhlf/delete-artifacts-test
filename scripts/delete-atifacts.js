c6d230c4ec4fb2ca97fb3f86f711e5a4f71ea9b1
mohakhlf@ <mohakhellafi@gmail.com>
2022-06-22 16:58:49 +0200

    feat: split between develop and tags artifacts to delete

--- scripts/delete-atifacts.js
+++ scripts/delete-atifacts.js
@@ -1,20 +1,22 @@
 module.exports = async({ github, context }) => {
-    console.log('context', context.payload.inputs)
-        // console.log('inputs', context)
-        // const artifacts_responds = await github.rest.actions.listArtifactsForRepo({
-        //     owner: context.repo.owner,
-        //     repo: context.repo.repo,
-        // });
+    if (context.payload.inputs.artifacts_to_delete === "develop") {
+        const artifacts_responds = await github.rest.actions.listArtifactsForRepo({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+        });
 
-    // const artifacts_list_with_develop = artifacts_responds.data.artifacts.filter(
-    //     (artifact) => artifact.name === 'main'
-    // )
-    // artifacts_list_with_develop.shift()
-    // artifacts_list_with_develop.forEach(async(artifact) => {
-    //     await github.rest.actions.deleteArtifact({
-    //         owner: context.repo.owner,
-    //         repo: context.repo.repo,
-    //         artifact_id: artifact.id,
-    //     });
-    // })
+        const artifacts_list_with_develop = artifacts_responds.data.artifacts.filter(
+            (artifact) => artifact.name === 'main'
+        )
+        artifacts_list_with_develop.shift()
+        artifacts_list_with_develop.forEach(async(artifact) => {
+            await github.rest.actions.deleteArtifact({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                artifact_id: artifact.id,
+            });
+        })
+    } else if (context.payload.inputs.artifacts_to_delete === "tags") {
+        console.log(context.payload.inputs.artifacts_to_delete)
+    }
 }
