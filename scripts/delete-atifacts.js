bf56d9b34d4dedc386bd59b9f3c45f38c8fc9482
mohakhlf@ <mohakhellafi@gmail.com>
2022-06-22 08:02:10 +0200

    fix(delete artifacts): :bug: tryfix

--- scripts/delete-atifacts.js
+++ scripts/delete-atifacts.js
@@ -1,8 +1,9 @@
 module.exports = async({ github, context }) => {
-    const artifacts_list = await github.rest.actions.listArtifactsForRepo({
+    const artifacts_list = await github.request(`GET /repos/${context.repo.owner}/${context.repo.repo}/actions/artifacts`, {
         owner: context.repo.owner,
         repo: context.repo.repo,
-    });
+    })
+
 
     console.log('artifacts_list', artifacts_list);
 }
