801e4fa3c8829de612c1e5e699ad9304af2e7687
mohakhlf@ <mohakhellafi@gmail.com>
2022-06-22 07:30:03 +0200

    feat(delete artifacts): :bug: tryfix

--- scripts/delete-atefacts.js
+++ scripts/delete-atefacts.js
@@ -1,6 +1,2 @@
-const artifacts_to_delete = await github.rest.actions.listArtifactsForRepo({
-    owner: owner,
-    repo: repo,
-})
-
-console.log('artifacts_to_delete', artifacts_to_delete)
+console.log('context: '
+    context)
